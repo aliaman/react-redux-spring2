c5cf2129df61de32eb650302417b1d52f2a1f6a8
ali_jalbani <Ali_Jalbani@symantec.com>
2017-05-02 13:34:29 -0700

    integrated login with backend

--- src/main/resources/sql/dml.sql
+++ src/main/resources/sql/dml.sql
@@ -1,6 +1,6 @@
-INSERT INTO ci_data.s_roles (id, name, display, lastUpdated)
-  VALUES (1, 'ADMINISTRATOR', 'Administrator', '2017-04-25 21:35:44'),
-  (2, 'SUPER_USER', 'Super User', '2017-04-25 21:35:44');
+INSERT INTO ci_cynic.s_roles (id, name, display, lastUpdated)
+VALUES (1, ''ADMINISTRATOR'', ''Administrator'', ''2017-04-25 21:35:44''),
+  (2, ''SUPER_USER'', ''Super User'', ''2017-04-25 21:35:44'');
 
-INSERT INTO ci_data.t_users (email, password, name, lastUpdated, role_id)
-  VALUES ('ali_jalbani@symantec.com', '', 'Ali Jalbani', '2017-04-25 21:35:45', 1);
+INSERT INTO ci_cynic.t_users (email, password, name, lastUpdated, role_id)
+VALUES (''ali_jalbani@symantec.com'', '''', ''Ali Jalbani'', ''2017-04-25 21:35:45'', 1);

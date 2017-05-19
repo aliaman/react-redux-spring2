a2727ed0dd62251da0c6d47b2c69a232266e7d41
ali_jalbani <Ali_Jalbani@symantec.com>
2017-05-19 11:46:14 -0700

    made suggestion work with table.... the problem was in the getPropForKey function

--- src/main/webapp/client/scomponents/Dashboard2.js
+++ src/main/webapp/client/scomponents/Dashboard2.js
@@ -6,17 +6,20 @@ import ReactTable from 'react-table'
 import ReactSpinner from 'reactjs-spinner'
 import Suggestion from './autosuggest/Suggestion'
 import * as RB from 'react-bootstrap'
+import update from 'react-addons-update'
 
 @connect((store) => {
     return {
-        data: store.hashTracking.data,
-        selectedHash: store.hashTracking.selectedHash
+        data: store.hashTracking.data
     }
 })
 class Dashboard2 extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            formatteddata:{
+                list: []
+            },
             autosuggest:{
                 reason: [{
                             name: "Reason1",
@@ -116,12 +119,23 @@ class Dashboard2 extends React.Component {
         this.setState({showModal: false});
     }
     componentWillReceiveProps(nextProps){
-        this.setState(nextProps);
+        let formatteddata = update(this.state.formatteddata,
+            {
+                $merge: {
+                    list: nextProps.data
+                }
+            }
+        );
+        formatteddata = this.addSupplementInfo(formatteddata);
+        console.log(formatteddata);
+        this.setState({
+            formatteddata: formatteddata
+        });
     }
     handleClick(id, event){
         this.setState({
             selectedHash: this.props.data.filter(hash => hash._id == id)[0]
-        })
+        });
         this.open();
     }
     close(){
@@ -130,12 +144,36 @@ class Dashboard2 extends React.Component {
     open(){
         this.setState({ showModal: true });
     }
-    getPropForKey(key, prop){
-        //TODO: filter the data for the key
-        return '';
+    getPropForKey(id, key){
+        let obj = this.state.formatteddata.list.filter((record)=>record._id == id)[0];
+        return (obj[key]==undefined?'':obj[key]);
     }
     autoSuggestChanged(id, value){
+        //TODO: persist
         console.log(`Autosuggest changed ${id} and ${value}...`)
+    }
+    addSupplementInfo(data){
+        const mockComments = [{
+            id: 'AVpsJE09t84KCCnldNvB',
+            reason: 'Reason reason reason',
+            comment: 'RandomComment'
+        },{
+            id: 'AVpr8JKVq-zzx-zFhWpZ',
+            reason: 'Reason2 reason reason'
+        }];
+
+        mockComments.forEach((mockComment)=>{
+            let id = mockComment.id;
+            let reason = mockComment.reason;
+            let comment = mockComment.comment;
+            for(let k in data.list){
+                if(data.list[k]._id == id){
+                    data.list[k]['reason'] = reason;
+                    data.list[k]['comment'] = comment;
+                }
+            }
+        });
+        return data;
     }
     render() {
         const style = {
@@ -195,25 +233,31 @@ class Dashboard2 extends React.Component {
         },{
             Header: 'Comment',
             accessor: '_id',
-            // Cell: props =>
-            //     <div>
-            //         <Suggestion
-            //             ref={(fieldEditor1) => {this.fieldEditor1 = fieldEditor1;}}
-            //             suggestions={this.state.autosuggest.comment} />
-            //     </div>
+            Cell: props =>
+                <div>
+                    <Suggestion
+                        id={props.value}
+                        reportChange={this.autoSuggestChanged.bind(this)}
+                        value={this.getPropForKey(props.value, "comment")}
+                        suggestions={this.state.autosuggest.comment} />
+                </div>
 
         },{
             Header: 'Mitigation',
             accessor: '_id',
-            // Cell: props =>
-            //     <div>
-            //         <Suggestion suggestions={this.state.autosuggest.mitigation} />
-            //     </div>
+            Cell: props =>
+                <div>
+                    <Suggestion
+                        id={props.value}
+                        reportChange={this.autoSuggestChanged.bind(this)}
+                        value={this.getPropForKey(props.value, "mitigation")}
+                        suggestions={this.state.autosuggest.mitigation} />
+                </div>
         },{
-                Header: '',
-                accessor: '_id',
-                Cell: props =>
-                    <span className='center-block'>
+            Header: '',
+            accessor: '_id',
+            Cell: props =>
+                <span className='center-block'>
                         <RB.Button className="inline__edit" onClick={this.handleClick.bind(this, props.value)}>
                             <span className="glyphicon glyphicon-edit"></span>
                         </RB.Button>
@@ -226,7 +270,7 @@ class Dashboard2 extends React.Component {
                     <h4 className="compactHeading">Sample Hash Tracking</h4>
                     <ReactTable
                         className="-striped -highlight"
-                        data={this.props.data}
+                        data={this.state.formatteddata.list}
                         columns={columns}
                         defaultPageSize={10}
                     />
